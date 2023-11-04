860d3001348e85d466d0e5012760687cd6545253
hrfahimm <fahimtaj10@gmail.com>
2023-11-05 00:02:49 +0600

    'wwww'

--- NextJS/try-next-js/src/components/HomePage/SingleCategory.jsx
+++ NextJS/try-next-js/src/components/HomePage/SingleCategory.jsx
@@ -4,14 +4,8 @@ import Link from "next/link";
 import React from "react";
 
 const SingleCategory = ({ category }) => {
-    const { id, title, name } = category;
-    return (
-        <div>
-            <Link href={`/products?categoryId=${id}`}>
-                <h1>*{name}</h1>
-            </Link>
-        </div>
-    );
+    const { id, name } = category;
+    return <Link href={`/products?categoryId=${id}`}>{name}</Link>;
 };
 
 export default SingleCategory;
